89d850167e8d38194d07b614731c69a34e1b0b69
dayfeather920317-bit <dayfeather920317@gmail.com>
2026-04-01 16:50:30 +0800

    fix last_user update

--- count_feature.py
+++ count_feature.py
@@ -41,7 +41,7 @@ async def handle_count(message, channel_id):
         expected = data["count"] + 1
         last_user = data.get("last_user")
 
-        # ❌ 同一個人連續數
+        # 同一個人不能連續數
         if last_user == user_id:
             data["count"] = 0
             data["last_user"] = None
@@ -51,16 +51,20 @@ async def handle_count(message, channel_id):
             await message.channel.send("不能連續數！重來 👊")
             return
 
+        # 數字正確
         if number == expected:
             data["count"] = number
             data["last_user"] = user_id
             save_count(data)
+
             await message.add_reaction("✅")
             await message.channel.send("你好棒👍數對了")
 
+        # 數字錯誤
         else:
             data["count"] = 0
-            data["last_user"] = user_id
+            data["last_user"] = None
             save_count(data)
+
             await message.add_reaction("❌")
             await message.channel.send("敢數錯?咬爆你喔 <a:emoji_R4:1408487451424587897>")
