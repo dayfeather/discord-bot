08d6be3393a1bb2a962110947151470051ed9120
dayfeather920317-bit <dayfeather920317@gmail.com>
2026-04-07 14:53:29 +0800

    update

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class MyClient(discord.Client):
 
 client = MyClient(intents=intents)
 
-@tasks.loop(time=time(hour=14, minute=53, tzinfo=ZoneInfo("Asia/Taipei")))
+@tasks.loop(time=time(hour=14, minute=56, tzinfo=ZoneInfo("Asia/Taipei")))
 async def birthday_check_loop():
     print("birthday_check_loop triggered")
 
